9167bcfb4739533a1a0c1dceda20917bfba2f930
Camya <camyacodes@gmail.com>
2022-04-18 19:07:38 -0400

    sum of 2 nums challenge

---
+++ sum_function.js
@@ -0,0 +1,9 @@
+// Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and returns their sum (e.g. addition(3, 2) ➞ 5)
+
+function add(num1, num2) {
+    var sum = num1 + num2
+    return sum
+}
+
+console.log(add(1, 2))
