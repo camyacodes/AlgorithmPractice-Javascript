09aef12dfbf1fc74df136152ac4a307756132337
Camya <camyacodes@gmail.com>
2022-04-20 20:26:37 -0400

    return the 1st elem in an array problem solved

---
+++ first_elem_arr.js
@@ -0,0 +1,8 @@
+//Return the First Element in an Array
+//Create a function that takes an array containing only numbers and return the first element (e.g. getFirstValue([1, 2, 3]) ➞ 1)
+
+function getFirstValue(arr) {
+    return arr[0]
+}
+
+console.log(getFirstValue([10, 2, 3]))
